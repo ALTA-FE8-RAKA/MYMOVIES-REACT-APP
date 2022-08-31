0c4c3e8aac14e0cfa73362ecc34d7189b766691f
Raka Fantino <rakafantino@gmail.com>
2022-09-01 02:00:54 +0700

    Mengubah Tampilan Menjadi Dark Mode Dan Mengganti Style Navbar

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -102,7 +102,7 @@ export default class Body extends Component {
 
               <a
                 href={url}
-                className="inline-block text-gray-800 dark:text-white bg-white dark:bg-gray-500 hover:bg-gray-100 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
+                className="inline-block text-gray-800 dark:text-white bg-white dark:bg-gray-500 hover:bg-gray-100 dark:hover:bg-gray-700 active:bg-gray-200 focus-visible:ring ring-indigo-300 border text-gray-500 text-sm md:text-base font-semibold text-center rounded-lg outline-none transition duration-100 px-4 md:px-8 py-2 md:py-3"
               >
                 Show more
               </a>
