8d921ad146f935b0a7e1e8692cc71009820f7cb7
Raka Fantino <rakafantino@gmail.com>
2022-09-01 02:31:11 +0700

    Mengubah Tampilan Menjadi Dark Mode atau Light Mode Berdasarkan User System Theme Dan Mengganti Style Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,7 +10,7 @@ export default class Navbar extends Component {
         <nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 dark:bg-gray-900 drop-shadow-md">
           <div className="container flex flex-wrap justify-between items-center mx-auto">
             <a href={url} className="flex items-center">
-              <FilmIcon className="mr-3 h-6 sm:h-9 dark:text-white" alt="Movie & Nyantuy Logo" />
+              <FilmIcon className="mr-3 h-6 sm:h-9 text-indigo-700 dark:text-white" alt="Movie & Nyantuy Logo" />
               <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MOVIE & NYANTUY</span>
             </a>
             <div className="flex md:order-2">
