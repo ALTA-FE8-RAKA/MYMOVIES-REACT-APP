2e58c5e79637b32d5e0a0066fbbffc3515ee35ec
Raka Fantino <rakafantino@gmail.com>
2022-09-04 18:43:29 +0700

    Commit Penambahan Fitur Toggle Dark Mode dan Detail Page

--- src/pages/Details.jsx
+++ src/pages/Details.jsx
@@ -9,12 +9,16 @@ class Details extends Component {
           <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
             <div className=" container mb-5 max-w-full justify-center gap-6 lg:flex">
               <div className=" flex basis-1/2 justify-center p-16 text-center text-2xl font-bold text-white">
-                <div className="container">
-                  <img src={this.props.location.state.image} alt="Random Images" className="self-center rounded-lg border-8 border-indigo-500 drop-shadow-xl dark:border-slate-600 " />
+                <div className="container group">
+                  <img
+                    src={this.props.location.state.image}
+                    alt="Random Images"
+                    className="self-center rounded-lg border-8 border-indigo-500 object-cover object-center drop-shadow-xl transition duration-200 group-hover:scale-110 dark:border-slate-600 "
+                  />
                 </div>
               </div>
-              <div className="basis-2/5 rounded bg-indigo-500 shadow-lg dark:bg-slate-700">
-                <ul className="flex flex-col text-slate-700 dark:text-neutral-300 sm:gap-y-11 sm:py-11 sm:px-5">
+              <div className="flex basis-2/5 rounded bg-indigo-500 shadow-lg drop-shadow-lg dark:bg-slate-700">
+                <ul className="flex flex-col gap-y-6 py-10 px-5 text-slate-700 dark:text-neutral-300">
                   <li className="text-2xl font-bold tracking-wider text-white dark:text-neutral-300">{this.props.location.state.title}</li>
                   <hr className="border-t-2 border-white  p-0 dark:border-slate-500 " />
                   <li className="text-white dark:text-neutral-300">
